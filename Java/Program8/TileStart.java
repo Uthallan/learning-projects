cd91c5d4c29a0baf523d1638a403f7a7eaa63c3f
Uthallan <81986073+Uthallan@users.noreply.github.com>
2021-11-17 12:26:50 -0500

    Program8_complete

--- Java/Program8/TileStart.java
+++ Java/Program8/TileStart.java
@@ -3,13 +3,23 @@ import java.awt.*;
 public class TileStart {
 
     public static void main(String[] args) {
-        DrawingPanel panel = new DrawingPanel(310,190);
-        Graphics2D g = panel.getGraphics();        
+        int RESOLUTION = 500;
+        int size = RESOLUTION/10;
+        int x = 0;
+        int y = 0;
+        DrawingPanel panel = new DrawingPanel(RESOLUTION,RESOLUTION);
+        Graphics2D g = panel.getGraphics();
 
-        diagonalTile(g, 10,10, 50);     // small
-        diagonalTile(g, 70,10, 50);     // small
-        diagonalTile(g, 10,70, 110);    // medium
-        diagonalTile(g, 130,10, 170);   // large
+
+        for (int i = 0; i <= 10; i++) {
+            int dx = RESOLUTION * i/10;
+            
+            for (int ii = 0; ii <= 10; ii++) {
+                int dy = RESOLUTION * ii/10;
+                if ((ii % 2) == 0) {targetTile(g, x+dx, y+dy, size);}
+                else {diagonalTile(g, x+dx, y+dy, size);}
+            }           
+        }      
     }
 
     public static void diagonalTile(Graphics2D g, int x, int y, int size) {
@@ -19,6 +29,16 @@ public class TileStart {
         for (int i = 0; i <= 4; i++) {
             int dx = size * i/5;
             g.drawLine(x, y+dx, x+size-dx, y+size);
+        }
+    }
+
+    public static void targetTile(Graphics2D g, int x, int y, int size) {
+        g.setColor(Color.BLACK);
+        g.drawRect(x, y, size, size);
+
+        for (int i = 0; i <= 4; i++) {
+            int dx = size * i/5;
+            g.drawOval(x+dx/2, y+dx/2, size-dx, size-dx);
         }
     }
 }
